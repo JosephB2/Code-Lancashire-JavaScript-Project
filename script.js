1b44af7ac30575625a9d29e063460a7960f298fd
Tim Aldred <tim5597@gmail.com>
2025-01-16 19:27:27 +0000

    Tim's upload

---
+++ script.js
@@ -0,0 +1,42 @@
+window.addEventListener("load", function(){
+    handleFormSubmit()
+})
+
+function handleFormSubmit(){
+    let form = document.getElementById("username-form")
+    form.addEventListener("submit", function(event){
+        event.preventDefault()
+
+        // creates a form data object providing the form element
+        let formData = new FormData(form)
+        // convert the form data into a usable object
+        let data = Object.fromEntries(formData.entries())
+
+        getUserDetailsFromGithub(data)
+    })
+}
+
+function getUserDetailsFromGithub(userInput){
+    fetch(`https://api.github.com/users/${userInput.username}`)
+    .then((response) => response.json())
+    .then((data) => {
+        let userimage = document.querySelector('#userimage')
+        let usernameTag = document.querySelector('#username')
+        let name = document.querySelector('#name')
+        let location = document.querySelector('#location')
+        let public_repos = document.querySelector('#public_repos')
+        let followers = document.querySelector('#followers')
+
+        usernameTag.innerHTML = data.login
+        name.innerHTML = data.name
+        location.innerHTML = data.location
+        public_repos.innerHTML = data.public_repos
+        followers.innerHTML = data.followers
+        userimage.setAttribute("src", data.avatar_url)
+
+        console.log('data', data)   
+
+    })
+
+}
+
