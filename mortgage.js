eaf675085f8fb74e94500891533b20d790d3ed43
Tim Aldred <tim5597@gmail.com>
2025-01-20 19:10:09 +0000

    Mortgag maths

--- mortgage.js
+++ mortgage.js
@@ -1,1 +1,46 @@
+/*
+Repayment Mortgage
+M = P [ i (1 + i)^n ] / [ (1 + i)^n – 1 ]
 
+M represents the monthly payment
+P is the principal (starting balance) of the loan
+i is the monthly interest rate
+n is the total number of payments (mortgage term in years multiplied by 12)
+
+}
+*/
+
+window.addEventListener("load", function() {
+    let startingBalance = 45000; 
+    let annualInterestRate = 3; 
+    let termYears = 10;
+
+    let monthlyInterestRate = annualInterestRate / 100 / 12;
+    let termMonths = termYears * 12;
+
+    let monthlyPayment = startingBalance *  (monthlyInterestRate * Math.pow(1 + monthlyInterestRate, termMonths)) / (Math.pow(1 + monthlyInterestRate, termMonths) - 1);
+    let totalPayment = monthlyPayment * termMonths;
+    let interestPaid = totalPayment - startingBalance;
+
+    console.log("Monthly Payment:", monthlyPayment); 
+    console.log("Total Payment:", totalPayment); 
+    console.log("Interest Paid:", interestPaid); 
+});
+
+/*
+Interest Only Mortgage
+*/
+window.addEventListener("load", function() {
+    let startingBalance = 45000; 
+    let annualInterestRate = 3; 
+    let termYears = 10;
+
+    let monthlyInterestRate = annualInterestRate / 100 / 12;
+    let termMonths = termYears * 12;
+
+    let monthlyPayment = startingBalance * monthlyInterestRate / 12
+    let totalPayment = monthlyPayment * termMonths;
+
+    console.log("Monthly Payment:", monthlyPayment); 
+    console.log("Total Payment:", totalPayment); 
+});
--- script.js
+++ script.js
@@ -1,14 +1,12 @@
 window.addEventListener("load", function(){
-    document.getElementById('info').style.visibility = 'hidden'
-    handleFormSubmit()
+/*    document.getElementById('info').style.visibility = 'hidden'  */
+handleFormSubmit()
 })
 
 function handleFormSubmit(){
     let form = document.getElementById("username-form")
     form.addEventListener("submit", function(event){
         event.preventDefault()
-        
-        
 
         // creates a form data object providing the form element
         let formData = new FormData(form)
@@ -17,18 +15,20 @@ function handleFormSubmit(){
 
         getUserDetailsFromGithub(data)
     })
-}
+}   
 
 function getUserDetailsFromGithub(userInput){
     fetch(`https://api.github.com/users/${userInput.username}`)
     .then((response) => response.json())
     .then((data) => {
-        let errortext = document.querySelector('#error')
+/*        let errortext = document.querySelector('#error') */
         let userimage = document.querySelector('#userimage')
         let usernameTag = document.querySelector('#username')
+        let name = document.querySelector('#name')
+        let location = document.querySelector('#location')
         let public_repos = document.querySelector('#public_repos')
         let followers = document.querySelector('#followers')
-
+/*
         // handling API errors        
         if(data.documentation_url == "https://docs.github.com/rest/overview/resources-in-the-rest-api#rate-limiting") { // 
             errortext.innerHTML = "Error: API limit exceeded"
@@ -46,18 +46,19 @@ function getUserDetailsFromGithub(userInput){
             showError(false)
             console.log('everythings fine')
         }
-
+*/
         usernameTag.innerHTML = data.login
+        name.innerHTML = data.name
+        location.innerHTML = data.location
         public_repos.innerHTML = data.public_repos
         followers.innerHTML = data.followers
         userimage.setAttribute("src", data.avatar_url)
-        
 
         console.log('data', data)
     })
 
 }
-
+/*
 function showError(isThereError) {
     if (isThereError == false) {
         document.getElementById('info').style.visibility = 'visible'
