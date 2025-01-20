2d5e1d9dd4e74804c74b44a7b570d704776317eb
JosephB2 <josephbousfield99@gmail.com>
2025-01-20 19:27:13 +0000

    Update GitHub app to functional state

--- githubscript.js
+++ githubscript.js
@@ -21,32 +21,32 @@ function getUserDetailsFromGithub(userInput){
     fetch(`https://api.github.com/users/${userInput.username}`)
     .then((response) => response.json())
     .then((data) => {
-/*        let errortext = document.querySelector('#error') */
+        let errortext = document.querySelector('#error')
         let userimage = document.querySelector('#userimage')
         let usernameTag = document.querySelector('#username')
         let name = document.querySelector('#name')
         let location = document.querySelector('#location')
         let public_repos = document.querySelector('#public_repos')
         let followers = document.querySelector('#followers')
-/*
+
         // handling API errors        
         if(data.documentation_url == "https://docs.github.com/rest/overview/resources-in-the-rest-api#rate-limiting") { // 
             errortext.innerHTML = "Error: API limit exceeded"
-            showError(true)
-            console.log('api activated')
+            //showError(true)
+            //console.log('api activated')
         }
         else if(data.login == null) {
             errortext.innerHTML = "Error: user not found"
-            showError(true)
-            console.log('undefined activated')
+            //showError(true)
+            //console.log('undefined activated')
 
         }
         else {
             errortext.innerHTML = null
-            showError(false)
-            console.log('everythings fine')
+            //showError(false)
+            //console.log('everythings fine')
         }
-*/
+
         usernameTag.innerHTML = data.login
         name.innerHTML = data.name
         location.innerHTML = data.location
@@ -69,3 +69,4 @@ function showError(isThereError) {
         document.getElementById('error').style.display = 'visible'
     }
 }
+    */
